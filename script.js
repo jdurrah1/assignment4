53e8b68c56fbd25420418b12db56e43ce1e19d7e
Durrah <jdurrah@aha15.ccs.itd.umich.edu>
2016-11-17 00:01:13 -0500

    pressing enter on search bar calls function now

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ var app = angular.module('493Search', []);
 app.controller('searchResult',[ '$scope', '$http', function($scope, $http) {
   // your code goes here
 
+  $scope.searchSpotify = function()
+  {
+
+  	$scope.searchValue = 'hi'; 
+  }
+
 }]);
 
 app.controller('similarArtistsCtrl',['$scope', '$http', function($scope, $http) {
