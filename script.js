205f45921f2534a7a4a8e5fded65876bcf7e9072
Durrah <jdurrah@aha1.ccs.itd.umich.edu>
2016-11-16 23:07:19 -0500

    initial push of starter code

---
+++ script.js
@@ -0,0 +1,11 @@
+var app = angular.module('493Search', []);
+
+app.controller('searchResult',[ '$scope', '$http', function($scope, $http) {
+  // your code goes here
+
+}]);
+
+app.controller('similarArtistsCtrl',['$scope', '$http', function($scope, $http) {
+  // your code goes here 
+
+}]);
